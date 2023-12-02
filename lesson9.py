6aa9bedda879b54fa0ba2a312aa618d2909c76e4
Baby3d <151312426+Baby3d@users.noreply.github.com>
2023-12-01 23:43:46 -0700

    Add files via upload

--- lesson9.py
+++ lesson9.py
@@ -6,46 +6,99 @@
 # >> 123432
 # Ответ не верный / Верный
 
+# mark = 0
 
-print("Welcome to our quiz. Answer 5 questions and we hope you enjoy the GAME) GOOD LUCK !")
-print("START")
-print("Capital of Ukraine?")
-answer = input()
-if answer == "Kyiv":
-    print("Good job")
-    print("Keep going")
-else:
-    print("Unfortunately the answer is not correct, try again!")
+# print("Welcome to our quiz. Answer 5 questions and we hope you enjoy the GAME) GOOD LUCK !")
+# print("START")
+# print("Capital of Ukraine?")
+# answer = input()
 
-print(" How much wiil 3 + 2 ")
-answer = input()
-if answer == "5":
-    print("This is cool ,keep it up!")
-else:
-    print("We're sorry ,try again(")
-print("What is the coldet ocean in the world ?")
-answer = input()
-if answer == "Arctic Ocean":
-    print("WOW, you are amazing!")
-else:
-    print("I'm sorry, try again!")
-
-print("What color will it be if you add red and yellow together?")
-answer = input()
-if answer == "orange":
-    print("Just a delight!")
-else:
-    print("We are very sorry,please try again!")
-print("One last question left") 
-print(" I believe in you!")
-print("When was the world's first smartphone released?")
-answer = input()
-if answer == "1994":
-    print("This is great!")
-    print("Thank you for participating!")
-else:
-    print("We are very soory, please try again!")
+# if answer == "Kyiv":
+#     print("Good job")
+#     print("1")
+#     print("Keep going")
+#     mark += 1
+# else:
+#     print("Unfortunately the answer is not correct, try again!")
 
 
 
- 
+
+# print(" How much wiil 3 + 2 ")
+# answer = input()
+# if answer == "5":
+    
+#     print("This is cool ,keep it up!")
+#     print("+1")
+#     mark += 1
+# else:
+#     print("We're sorry ,try again(")
+
+
+
+
+# print("What is the coldet ocean in the world ?")
+# answer = input()
+# if answer == "Arctic Ocean":
+#     print("WOW, you are amazing!")
+#     print("+1")
+#     mark += 1
+
+# else:
+#     print("I'm sorry, try again!")
+
+
+
+# print("What color will it be if you add red and yellow together?")
+# answer = input()
+# if answer == "orange":
+#     print("Just a delight!")
+#     print("+1")
+#     mark += 1
+# else:
+#     print("We are very sorry,please try again!")
+
+
+# print("One last question left") 
+# print(" I believe in you!")
+# print("When was the world's first smartphone released?")
+# answer = input()
+# if answer == "1994":
+#     print("This is great!")
+#     print("+1")
+#     mark += 1
+#     print("Thank you for participating!")
+
+# else:
+#     print("We are very soory, please try again!")
+
+
+# print("Your mark is: " + str(mark))
+#####################################################
+
+# football_winnings = int(input())#4 - > 4 * 3 -> 12
+# football_loss = int(input())#2 -> 2 * 0 = 0
+# football_dead_heat = int(input())#3 -> 3 * 1 -> 3
+
+# print("Please enter your team's points:")
+# if football_winnings >=3:
+#     print("Your team won, Congratulations!")
+
+# if football_loss == 0:
+#     print("We are very sorry ! Your team lost!")
+
+# if football_dead_heat == 1:
+#     print("You have a draw!") 
+#     print("Thank you very much for playing football!")       
+
+#############################################################3
+
+
+weight = int(input())
+#55
+if weight <= 60:
+    print ("You belong to the 'light weight' category." )
+if weight > 60 and weight <= 64:
+    print("You category is 'first welterweight'.") 
+if weight > 64 and weight <= 69:
+    print("Your category is 'welterweight.'")      
